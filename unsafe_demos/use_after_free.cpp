8306aa716592d825dbf220d7e7536a0647578cbc
LeeJeungHwa <ljh15952@gmail.com>
2026-07-24 14:34:06 +0900

    Delete unsafe_demos directory

--- unsafe_demos/use_after_free.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    int* value = new int{42};
-    delete value;
-
-    // Intentional undefined behavior for AddressSanitizer practice.
-    std::cout << *value << '\n';
-}
