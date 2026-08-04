f75d99490be5caee11c957833c8bf98d9a5b016f
LeeJeungHwa <ljh15952@gmail.com>
2026-08-04 11:04:30 +0900

    Add printArr function to display array elements

--- Practice/3.cpp
+++ Practice/3.cpp
@@ -22,3 +22,31 @@ int main() {
 	}
 	return 0;
 }
+
+#include "Player.h"
+#include <iostream>
+#include <vector>
+#include <span>
+using namespace std;
+
+
+void printArr(span<int> arr) {
+	for (auto& i : arr) {
+		cout << i << " ";
+		++i;
+	}
+	cout << endl;
+}
+
+int main() {
+	
+	int arr1[] = { 1, 2, 3, 4, 5 };
+	vector<int> arr2 = { 6, 7, 8, 9, 10 };
+
+	printArr(arr1);
+	printArr(arr2);
+
+	printArr(arr1);
+	printArr(arr2);
+	return 0;
+}
